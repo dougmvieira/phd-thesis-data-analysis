0d4647530dd27d64d410e435dad6f48e61096b9d
Doug V <dougmvieira@gmail.com>
2022-02-27 20:30:09 +0000

    Finish summary_stats.py

--- summary_stats.py
+++ summary_stats.py
@@ -3,7 +3,7 @@ import pandas as pd
 import xarray as xr
 
 
-def get_summary_stats_for_date(tick_quotes: np.ndarray) -> pd.DataFrame:
+def get_summary_stats_for_date(tick_quotes: np.ndarray) -> xr.Dataset:
     specs = tick_quotes['index'][['expiry', 'strike', 'payoff']]
     specs_uniq = np.concatenate([specs[:1], specs[1:][specs[1:] != specs[:-1]]])
     np.testing.assert_equal(specs_uniq, np.unique(specs_uniq))
@@ -16,17 +16,17 @@ def get_summary_stats_for_date(tick_quotes: np.ndarray) -> pd.DataFrame:
 
     return xr.Dataset(
         dict(
-            n_options=('expiry', n_options),
-            n_rows=('expiry', n_rows),
+            n_options=('expiry', [*n_options, np.sum(n_options)]),
+            n_rows=('expiry', [*n_rows, np.sum(n_rows)]),
         ),
         dict(
-            expiry=('expiry', expiries_fmt),
+            expiry=('expiry', [*expiries_fmt, 'total']),
             date=tick_quotes['date'],
         )
     )
 
 
-def get_summary_stats(*tick_quotes_lst: np.ndarray):
+def get_summary_stats(*tick_quotes_lst: np.ndarray) -> pd.DataFrame:
     summaries = xr.concat(
         [get_summary_stats_for_date(tick_quotes) for tick_quotes in tick_quotes_lst],
         'date',
@@ -34,11 +34,32 @@ def get_summary_stats(*tick_quotes_lst: np.ndarray):
     )
     return xr.Dataset(
         dict(
-            n_options_1st_quartile=summaries.n_options.quantile(0.25, 'date'),
+            n_options_min=summaries.n_options.min('date'),
             n_options_median=summaries.n_options.median('date'),
-            n_options_3rd_quartile=summaries.n_options.quantile(0.25, 'date'),
-            n_rows_1st_quartile=summaries.n_rows.quantile(0.25, 'date'),
+            n_options_mean=summaries.n_options.mean('date'),
+            n_options_max=summaries.n_options.max('date'),
+            n_rows_min=summaries.n_rows.min('date'),
             n_rows_median=summaries.n_rows.median('date'),
-            n_rows_3rd_quartile=summaries.n_rows.quantile(0.25, 'date'),
+            n_rows_mean=summaries.n_rows.mean('date'),
+            n_rows_max=summaries.n_rows.max('date'),
         ),
-    ).drop('quantile').to_dataframe()
+    ).to_dataframe()
+
+
+def tabulate_summary_stats(*tick_quotes_lst: np.ndarray):
+    return (
+        get_summary_stats(*tick_quotes_lst)
+        .rename(
+            columns=dict(
+                n_options_min="Active options\n(min)",
+                n_options_median="Active options\n(median)",
+                n_options_mean="Active options\n(mean)",
+                n_options_max="Active options\n(max)",
+                n_rows_min="Mid-price changes\n(min)",
+                n_rows_median="Mid-price changes\n(median)",
+                n_rows_mean="Mid-price changes\n(mean)",
+                n_rows_max="Mid-price changes\n(max)",
+            )
+        )
+        .to_latex()
+    )
