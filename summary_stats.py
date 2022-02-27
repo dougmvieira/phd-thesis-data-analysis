22bc2134bfeff289a5da029e096e18c8808d6aca
Doug V <dougmvieira@gmail.com>
2022-02-27 17:09:54 +0000

    Refactor summary_stats.py

--- summary_stats.py
+++ summary_stats.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import xarray as xr
 
 
 def get_summary_stats_for_date(tick_quotes: np.ndarray) -> pd.DataFrame:
@@ -13,17 +14,31 @@ def get_summary_stats_for_date(tick_quotes: np.ndarray) -> pd.DataFrame:
     np.testing.assert_array_equal(expiries, expiries_uniq)
     np.testing.assert_equal(np.sum(n_rows), len(tick_quotes['index']))
 
-    summary = pd.DataFrame(
-        dict(expiry=expiries_fmt, n_options=n_options, n_rows=n_rows)
-    ).set_index('expiry')
-    summary.loc['total'] = summary.sum(axis=0)
-    summary['date'] = tick_quotes['date']
-
-    return summary
+    return xr.Dataset(
+        dict(
+            n_options=('expiry', n_options),
+            n_rows=('expiry', n_rows),
+        ),
+        dict(
+            expiry=('expiry', expiries_fmt),
+            date=tick_quotes['date'],
+        )
+    )
 
 
 def get_summary_stats(*tick_quotes_lst: np.ndarray):
-    summary = pd.concat(
-        [get_summary_stats_for_date(tick_quotes) for tick_quotes in tick_quotes_lst]
+    summaries = xr.concat(
+        [get_summary_stats_for_date(tick_quotes) for tick_quotes in tick_quotes_lst],
+        'date',
+        fill_value=0,
     )
-    return summary.groupby('expiry').mean()
+    return xr.Dataset(
+        dict(
+            n_options_1st_quartile=summaries.n_options.quantile(0.25, 'date'),
+            n_options_median=summaries.n_options.median('date'),
+            n_options_3rd_quartile=summaries.n_options.quantile(0.25, 'date'),
+            n_rows_1st_quartile=summaries.n_rows.quantile(0.25, 'date'),
+            n_rows_median=summaries.n_rows.median('date'),
+            n_rows_3rd_quartile=summaries.n_rows.quantile(0.25, 'date'),
+        ),
+    ).drop('quantile').to_dataframe()
